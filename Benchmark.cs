c49d6731199c563cb6eddda3c9c7b730ba9feaf7
PabloHorno <pablojhorno@gmail.com>
2021-11-02 19:39:07 +0100

    Added Benchmark solution for .Net

---
+++ Benchmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class Benchmark
+    {
+        [Benchmark(Baseline = true)]
+        public int GetRandom_SystemRandom()
+        {
+            return new Random().Next();
+        }
+        
+        [Benchmark]
+        public int GetRandom_SystemSecurityCryptography()
+        {
+            return RandomNumberGenerator.GetInt32(int.MaxValue);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+﻿A simple dotnet solution to do .NET benchmarks using  [BenchmarkDotNet](https://github.com/dotnet/BenchmarkDotNet)
+
+# Run benchmark
+Simply run the following command in the solution directory.
+
+```cs
+dotnet run -c Release
+```
